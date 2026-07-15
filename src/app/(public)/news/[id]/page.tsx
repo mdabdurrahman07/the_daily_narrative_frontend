d6d49336a4a75025a432c5220070669e4132ef3d
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-15 23:25:59 +0600

    feat: auth, public, dashboard route implemented setup folder structure

---
+++ src/app/(public)/news/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsIdPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default NewsIdPage;
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
+const HomePage = () => {
   return <div className="text-4xl font-semibold text-center py-5">Home Page</div>;
 };
 
-export default Home;
+export default HomePage;
