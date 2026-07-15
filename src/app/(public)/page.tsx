7f8c986e6f3d1d8ebd681101f8c41ac26b806d0b
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-07-16 00:45:41 +0600

    shadcn implemented

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-4xl font-semibold text-center py-5">Home Page</div>;
+  return (
+    <div className="text-4xl font-semibold text-center py-5">
+      Home Page <Button variant={"secondary"}>Click Me</Button>
+    </div>
+  );
 };
 
 export default HomePage;
